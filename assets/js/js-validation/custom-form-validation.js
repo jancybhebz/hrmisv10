b91b2a35dde26fa0efec69d710ab2a4b7613d61f
MYCHLL <mychll31@gmail.com>
2018-06-04 15:04:40 +0800

    Add radio box required

--- assets/js/js-validation/custom-form-validation.js
+++ assets/js/js-validation/custom-form-validation.js
@@ -1,79 +1,92 @@
-function checkElement(e, obj = '') {
+/* settings:
+	<div class="form-group">
+		<label class="control-label">Label<span class="required"> * </span></label>
+		<div class="input-icon right">
+			<i class="fa fa-warning tooltips i-required"></i>
+			<input type="text" class="form-control form-required">
+		</div>
+	</div>
+	<div class="form-group">
+        <label class="control-label">Label</label>
+        <div class="radio-list radio-required">
+            <label class="radio-inline">
+                <input type="radio" name="radgender1"> Female </label>
+        </div>
+    </div>
+**/
+function checkElement(e,obj='',value=0)
+{
 	var res = 1;
-	if(e.val() == ''){
-		e.parent().parent().addClass('has-error');
-		e.prev("i").attr('data-original-title', "This field is required.");
-		e.prev("i").show();
-		res = 1;
+	if(obj=='radio'){
+		if(value == 1){
+			e.parent().removeClass('has-error');
+			res = 0;
+		}else{
+			e.parent().addClass('has-error');
+			res = 0;
+		}
 	}else{
-		if(obj == 'text'){
-	        if(!e.val().replace(/\s/g, '').length){
-	            e.parent().parent().addClass('has-error');
-				e.prev("i").attr('data-original-title', "Invalid input.");
-				e.prev("i").show();
-				res = 1;
-	        }else{
-	        	e.prev("i").hide();
+		if(e.val() == ''){
+			e.parent().parent().addClass('has-error');
+			e.prev("i").attr('data-original-title', "This field is required.");
+			e.prev("i").show();
+			res = 1;
+		}else{
+			if(obj == 'text'){
+			       if(!e.val().replace(/\s/g, '').length){
+			           e.parent().parent().addClass('has-error');
+					e.prev("i").attr('data-original-title', "Invalid input.");
+					e.prev("i").show();
+					res = 1;
+			       }else{
+			        e.prev("i").hide();
+					e.parent().parent().removeClass('has-error');
+					res = 0;
+			       }
+			   }else{
+			    e.prev("i").hide();
 				e.parent().parent().removeClass('has-error');
 				res = 0;
-	        }
-	    }else{
-	    	e.prev("i").hide();
-			e.parent().parent().removeClass('has-error');
-			res = 0;
-	    }
+			   }
+		}
 	}
 	return res;
 }
 
 $(document).ready(function() {
-
+	$('.i-required').hide();
 	if($('.loading-image').length > 0){
 		$('.loading-image').hide();
 	    $('.portlet-body').show();
 	}
 
-	$('form [type="text"]').keyup(function(e) {
+	$('form [type="text"].form-required').keyup(function(e) {
 		checkElement($(this), 'text');
 	});
 
-	$('form select').change(function(e) {
+	$('form select.form-required').change(function(e) {
 		checkElement($(this));
 	});
 
-	// if($('form [type="radio"]').length > 0){
-	// 	$('form [type="radio"]').click(function(e) {
-	// 		$('.radio-list').parent().parent().removeClass('has-error');
-	// 		$('.radio-list').parent().parent().find('.help-block').html('');
-	// 	});
-	// }
+	if($('form [type="radio"]').length > 0){
+		$('.radio-required').click(function() {
+			checkElement($(this), 'radio', $(this).find("input:radio:checked").length);
+		});
+	}
 
 	$('form').submit(function(e) {
 		var resval = [];
-		$('[type="text"]').each(function() {
+		$('[type="text"].form-required').each(function() {
 			resval.push(checkElement($(this), 'text'));
 		});
 
-		$('select').each(function() {
+		$('select.form-required').each(function() {
 			resval.push(checkElement($(this)));
 		});
 
-
-		// if($('form [type="radio"]').length > 0){
-		// 	radchoice = [];
-		// 	$('[type="radio"]').each(function() {
-		// 		if($(this).is(':checked')){
-		// 			radchoice.push($(this).val());
-		// 		}
-		// 	});
-		// 	if(radchoice.length < 1){
-		// 		$('[type="radio"]').each(function() {
-		// 			$('.radio-list').parent().parent().addClass('has-error');
-		// 			$('.radio-list').parent().parent().find('.help-block').html('This field is required.');
-		// 		});
-		// 	}
-		// 	resval.push(radchoice.length > 0 ? 0 : 1);
-		// }
+		$('.radio-required').each(function() {	
+			resval.push(checkElement($(this), 'radio', $(this).find("input:radio:checked").length));
+		});
 
 		resval = resval.slice(1);
 		if(resval.includes(1)){
