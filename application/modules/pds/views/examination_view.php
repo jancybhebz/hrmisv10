b84d26dff755a80fbf7b28e461910fd7b022a4ac
Rose Anne <rosegrefaldeo01@gmail.com>
2018-07-09 10:40:16 +0700

    Worked on modal (edit link) for Examination

--- application/modules/pds/views/examination_view.php
+++ application/modules/pds/views/examination_view.php
@@ -20,10 +20,206 @@
                         <td><?=$row['examPlace']?></td>
                         <td><?=$row['licenseNumber']?></td>
                         <td><?=$row['dateRelease']?></td>
-                        <td> <a href="<?=base_url('employees/profile/edit')?>"><button class="btn btn-sm btn-success"><span class="fa fa-edit" title="Edit"></span> Edit</button></a>
+                        <td>  <a class="btn green" data-toggle="modal" href="#exam_modal"> Edit </a>
                         <a href="<?=base_url('employees/profile/delete')?>"><button class="btn btn-sm btn-danger"><span class="fa fa-trash" title="Delete"></span> Delete</button></a></td>
                     </tr>
                     <?php endforeach;?>
             </table>
+             <div class="modal fade bs-modal-lg"  id="exam_modal" tabindex="-1" role="dialog" aria-hidden="true">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                            <h4 class="modal-title"><b>Examinations</b></h4>
+                        </div>
+                            <div class="modal-body"> </div>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <div class="form-group">
+                                    </div>
+                                </div>
+                                <div class="col-sm-3 text-left">
+                                    <div class="form-group">
+                                        <label class="control-label">Exam Description : <span class="required"> * </span></label>
+                                    </div>
+                                </div>
+                                <div class="col-sm-5" text-left>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="strExamDesc" value="<?=isset($arrData[0]['childName'])?$arrData[0]['childName']:''?>">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <div class="form-group">
+                                    </div>
+                                </div>
+                                <div class="col-sm-3 text-left">
+                                    <div class="form-group">
+                                        <label class="control-label">Rating : <span class="required"> * </span></label>
+                                    </div>
+                                </div>
+                                <div class="col-sm-5" text-left>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="strRating" value="<?=isset($arrData[0]['childName'])?$arrData[0]['childName']:''?>">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <div class="form-group">
+                                    </div>
+                                </div>
+                                <div class="col-sm-3 text-left">
+                                    <div class="form-group">
+                                        <label class="control-label">Place of Exam/Confernment : <span class="required"> * </span></label>
+                                    </div>
+                                </div>
+                                <div class="col-sm-5" text-left>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="strLicense" value="<?=isset($arrData[0]['childName'])?$arrData[0]['childName']:''?>">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <div class="form-group">
+                                    </div>
+                                </div>
+                                <div class="col-sm-3 text-left">
+                                    <div class="form-group">
+                                        <label class="control-label">License : <span class="required"> * </span></label>
+                                    </div>
+                                </div>
+                                <div class="col-sm-5" text-left>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="strLicense" value="<?=isset($arrData[0]['childName'])?$arrData[0]['childName']:''?>">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <div class="form-group">
+                                    </div>
+                                </div>
+                                <div class="col-sm-3 text-left">
+                                    <div class="form-group">
+                                        <label class="control-label">Date of Validity : <span class="required"> * </span></label>
+                                    </div>
+                                </div>
+                                <div class="col-sm-5" text-left>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="strValidity" value="<?=isset($arrData[0]['childName'])?$arrData[0]['childName']:''?>">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn dark btn-outline" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn green">Save changes</button>
+                        </div>
+                    </div>
+                    <!-- /.modal-content -->
+                </div>
+            <!-- /.modal-dialog -->
+        </div>
+
+         <a class="btn green" data-toggle="modal" href="#addExam_modal"> Add </a>
+         <div class="modal fade bs-modal-lg"  id="addExam_modal" tabindex="-1" role="dialog" aria-hidden="true">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                            <h4 class="modal-title"><b>Examinations</b></h4>
+                        </div>
+                            <div class="modal-body"> </div>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <div class="form-group">
+                                    </div>
+                                </div>
+                                <div class="col-sm-3 text-left">
+                                    <div class="form-group">
+                                        <label class="control-label">Exam Description : <span class="required"> * </span></label>
+                                    </div>
+                                </div>
+                                <div class="col-sm-5" text-left>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="strExamDesc" value="<?=isset($arrData[0]['childName'])?$arrData[0]['childName']:''?>">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <div class="form-group">
+                                    </div>
+                                </div>
+                                <div class="col-sm-3 text-left">
+                                    <div class="form-group">
+                                        <label class="control-label">Rating : <span class="required"> * </span></label>
+                                    </div>
+                                </div>
+                                <div class="col-sm-5" text-left>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="strRating" value="<?=isset($arrData[0]['childName'])?$arrData[0]['childName']:''?>">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <div class="form-group">
+                                    </div>
+                                </div>
+                                <div class="col-sm-3 text-left">
+                                    <div class="form-group">
+                                        <label class="control-label">Place of Exam/Confernment : <span class="required"> * </span></label>
+                                    </div>
+                                </div>
+                                <div class="col-sm-5" text-left>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="strLicense" value="<?=isset($arrData[0]['childName'])?$arrData[0]['childName']:''?>">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <div class="form-group">
+                                    </div>
+                                </div>
+                                <div class="col-sm-3 text-left">
+                                    <div class="form-group">
+                                        <label class="control-label">License : <span class="required"> * </span></label>
+                                    </div>
+                                </div>
+                                <div class="col-sm-5" text-left>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="strLicense" value="<?=isset($arrData[0]['childName'])?$arrData[0]['childName']:''?>">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <div class="form-group">
+                                    </div>
+                                </div>
+                                <div class="col-sm-3 text-left">
+                                    <div class="form-group">
+                                        <label class="control-label">Date of Validity : <span class="required"> * </span></label>
+                                    </div>
+                                </div>
+                                <div class="col-sm-5" text-left>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="strValidity" value="<?=isset($arrData[0]['childName'])?$arrData[0]['childName']:''?>">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn dark btn-outline" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn green">Save changes</button>
+                        </div>
+                    </div>
+                    <!-- /.modal-content -->
+                </div>
+            <!-- /.modal-dialog -->
+        </div>
     </form>
 </div>
