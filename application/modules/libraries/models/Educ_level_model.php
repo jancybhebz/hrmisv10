b93dca221d5af2f72a9a939d81b01d70eaae8a2d
Rose Anne <rosegrefaldeo01@gmail.com>
2019-03-26 14:05:25 +0700

    Change order by leve to levelcode

--- application/modules/libraries/models/Educ_level_model.php
+++ application/modules/libraries/models/Educ_level_model.php
@@ -17,7 +17,7 @@ class Educ_level_model extends CI_Model {
 	
 	function getData($intEducLevelId = '')
 	{
-		$this->db->order_by('level', 'desc');
+		$this->db->order_by('levelCode', 'desc');
 		if($intEducLevelId != ""):
 			return $this->db->get_where('tblEducationalLevel', array('levelId' => $intEducLevelId))->result_array();
 		else:
