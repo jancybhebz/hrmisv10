cdb725e823ee1d40ea770f2b83fc49a2172ea040
MYCHLL <mychll31@gmail.com>
2019-04-02 10:51:23 +0800

    move buttons to action button

--- application/modules/hr/views/attendance/attendance_summary/_dtr.php
+++ application/modules/hr/views/attendance/attendance_summary/_dtr.php
@@ -48,32 +48,6 @@
                         <a class="btn blue pull-right" data-toggle="modal" data-target="#print-preview-modal">Preview / Print</a>
                     <?php endif; ?>
                 </div>
-                <!-- <?php if( $_SESSION['sessUserLevel'] == 1): ?>
-                    <br><br>
-                    <p><center>
-                        <div class="form-inline" style="line-height: 3;">
-                            <a class="btn blue"
-                                href="<?=base_url('hr/attendance_summary/dtr/edit_mode').'/'.$arrData['empNumber'].'?yr='.$yr.'&month='.$month?>">Edit Mode</a>&nbsp;
-                            <a class="btn blue"
-                                href="<?=base_url('hr/attendance_summary/dtr/broken_sched').'/'.$arrData['empNumber']?>">Broken Sched</a>&nbsp;
-                            <a class="btn blue"
-                                href="<?=base_url('hr/attendance_summary/dtr/local_holiday').'/'.$arrData['empNumber']?>">Local Holiday</a>&nbsp;
-                            <a class="btn blue"
-                                href="<?=base_url('hr/attendance_summary/dtr/ob').'/'.$arrData['empNumber']?>">OB</a>&nbsp;
-                            <a class="btn blue"
-                                href="<?=base_url('hr/attendance_summary/dtr/leave').'/'.$arrData['empNumber']?>">Leave</a>&nbsp;
-                            <a  class="btn blue"
-                                href="<?=base_url('hr/attendance_summary/dtr/compensatory_leave').'/'.$arrData['empNumber']?>">Compensatory Leave</a>&nbsp;
-                            <a class="btn blue"
-                                href="<?=base_url('hr/attendance_summary/dtr/time').'/'.$arrData['empNumber']?>">Time</a>&nbsp;
-                            <a class="btn blue"
-                                href="<?=base_url('hr/attendance_summary/dtr/flagcrmy').'/'.$arrData['empNumber']?>">Flag Ceremony</a>&nbsp;
-                            <a class="btn blue"
-                                href="<?=base_url('hr/attendance_summary/dtr/to').'/'.$arrData['empNumber']?>">Travel Order</a>&nbsp;
-                            <a class="btn blue" data-toggle="modal" data-target="#print-preview-modal">Preview / Print</a>
-                        </div>
-                    </p></center>
-                <?php endif; ?> -->
             </div>
             <div style="display: inline-flex;">
                 <div class="legend-def1">
