92c615306d392e32f36bafc94734a2b691c85170
Mai <mychll31@gmail.com>
2019-04-26 01:14:49 +0800

    fix login when session is true

--- application/modules/login/controllers/Login.php
+++ application/modules/login/controllers/Login.php
@@ -9,11 +9,13 @@ class Login extends MY_Controller {
 
 	public function index()
 	{
+		$session = isset($_SESSION['sessBoolLoggedIn']) ? 1 : 0;
+		$session = $session ? $_SESSION['sessBoolLoggedIn'] : 0;
+
 		$arrPost = $this->input->post();
 		if(!empty($arrPost)):
 			$this->load->model('login_model');
 			$arrUser = $this->login_model->authenticate($arrPost['strUsername'],$arrPost['strPassword']);
-			//print_r($arrUser);exit(1);
 			if(!empty($arrUser)):
 				
 				$this->set_session_login_data($arrUser[0]['empNumber'],$arrUser[0]['userLevel'],$arrUser[0]['userPermission'],$arrUser[0]['accessPermission'],$arrUser[0]['userName'],$arrUser[0]['userPassword'],$arrUser[0]['firstname'].' '.$arrUser[0]['surname'],$arrUser[0]['assignedGroup']);
@@ -22,9 +24,12 @@ class Login extends MY_Controller {
 				$this->session->set_flashdata('strErrorMsg','Invalid username/password.');
 				$this->load->view('login_view');
 			endif;
-			//print_r($arrPost);
 		else:
-			$this->load->view('login_view');
+			if(!$session):
+				$this->load->view('login_view');
+			else:
+				redirect('home');
+			endif;
 		endif;
 	}
 
