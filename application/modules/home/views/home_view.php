216539da71e851b37fa744689315b2f331e662f6
MYCHLL <mychll31@gmail.com>
2019-04-29 09:00:03 +0800

    move chart from pds to dashboard

--- application/modules/home/views/home_view.php
+++ application/modules/home/views/home_view.php
@@ -153,5 +153,252 @@
 </div>
 <div class="clearfix"></div>
 <!-- END APPOINTMENT STATS -->
+
+
+<div class="row">
+    <div class="col-md-6 col-sm-6">
+        <!-- BEGIN PORTLET-->
+        <div class="portlet light bordered">
+            <div class="portlet-title">
+                <div class="caption">
+                    <i class="icon-bar-chart font-green"></i>
+                    <span class="caption-subject font-green bold uppercase">Plantilla Positions</span>
+                </div>
+            </div>
+            <div class="portlet-body">
+                <div id="chart_plantilla"></div>
+            </div>
+        </div>
+        <!-- END PORTLET-->
+    </div>
+    <div class="col-md-6 col-sm-6">
+        <!-- BEGIN PORTLET-->
+        <div class="portlet light bordered">
+            <div class="portlet-title">
+                <div class="caption">
+                    <i class="icon-share font-red-sunglo hide"></i>
+                    <span class="caption-subject font-red-sunglo bold uppercase">Gender</span>                
+                </div>
+            </div>
+            <div class="portlet-body">
+                <?php //print_r($arrGenderChart);
+                //echo "<br><br>";
+                //print_r($arrAS);
+                //echo "<br><br>";
+                $str = 'data: [';$i=0;//$intTotalMale=0;
+                foreach($arrASFull as $row):
+                    $strAppCode = $arrGenderChart[$row]['M']['appCode'];
+                    $arrGenderAS[$strAppCode]['M'] = 0;
+                    $arrGenderAS[$strAppCode]['F'] = 0;
+                endforeach;
+                foreach($arrASFull as $row):
+                    $intTotalMale = $arrGenderChart[$row]['M'][0]['total'];
+                    $intTotalFemale = $arrGenderChart[$row]['F'][0]['total'];
+                    $strAppCode = $arrGenderChart[$row]['M']['appCode'];
+                    //echo $row."<br>";
+                    //echo $row.'=>'.$strAppCode.'=>M('.$intTotalMale.") F(".$intTotalFemale.")<br><br>";
+                   // echo $arrGenderChart[]
+                    $arrGenderAS[$strAppCode]['M']+=$intTotalMale;
+                    $arrGenderAS[$strAppCode]['F']+=$intTotalFemale;
+                        //$intTotalMale += $arrGenderChart[$row]['M'][0]['total'];
+                        //echo $arrGenderChart[$row]['M'][0]['total']."<br>";
+                        $str .= $i>0?',':'';
+                        $str .= 'data: ['.$arrGenderAS[$strAppCode]['M'].', 105, 85, 70]';
+                endforeach;
+
+                $str .= ']';
+                //echo $intTotalMale;
+                //echo $str;
+                //print_r($arrGenderAS);
+                //echo "<br>";
+                ?>
+                <div id="chart_gender">
+                    
+                </div>
+                
+            </div>
+        </div>
+        <!-- END PORTLET-->
+    </div>
+</div>
+<?=load_plugin('js',array('datatables','highcharts'));?>
+
+<script>
+    $(document).ready(function() {
+        $('#tblemployees').dataTable( {
+            "initComplete": function(settings, json) {
+                $('.loading-image').hide();
+                $('#tblemployees').show();
+            }, pageLength: 5,} );
+
+        // Build the chart
+        $('#chart_plantilla').highcharts({
+            chart: {
+                height:250,
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false
+            },
+            title: {
+                text: ''
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        color: '#000000',
+                        connectorColor: '#000000',
+                        formatter: function() {
+                            return '<b>'+ this.point.name +'</b>';//: '+ this.percentage +' %
+                        }
+                    }
+                }
+            },
+            series: [{
+                type: 'pie',
+                name: 'Plantilla Positions',
+                data: [
+                    ['Filled',   <?=$intFilled?>],
+                    ['Vacant',   <?=$intVacant?>]
+                ]
+            }]
+        });
+
+        $('#chart_gender').highcharts({       
+            chart: {                
+                height:250
+            },
+            title: {
+                text: ''
+            },
+            xAxis: {
+                <?php 
+                $str = "categories: [";$i=0;
+                foreach($arrAS as $row):
+                    $str .= ($i>0?',':'')."'".appstatus_name($row)."'";
+                    $i++;
+                endforeach;
+                $str .= "]";
+                echo $str;
+                ?>
+                // categories: ['Contractual','Job Order','Permanent']
+            },
+            tooltip: {
+                formatter: function() {
+                    var s;
+                    if (this.point.name) { // the pie chart
+                        s = ''+
+                            this.point.name +': '+ this.y +' ';
+                    } else {
+                        s = ''+
+                            this.x  +': '+ this.y;
+                    }
+                    return s;
+                }
+            },
+            yAxis: {
+                title: {
+                    text: 'Employees'
+                }
+            },
+            labels: {
+                items: [{
+                    html: ' ',
+                    style: {
+                        left: '40px',
+                        top: '8px',
+                        color: 'black'
+                    }
+                }]
+            },
+            series: [{
+                type: 'column',
+                name: 'Male',
+                /*data: [<? for($i=0;$i<count($categories);$i++){
+                        echo ($maledata[$i]=="")?0:$maledata[$i];
+                        $totalmale+=$maledata[$i];
+                        if($i<count($categories)-1) echo ",";
+                    }?>]
+                    */
+                <?php 
+                    $str = 'data: [';
+                    $i=0;
+                    foreach($arrAS as $row):
+                        $strAppCode = $arrGenderChart[$row]['M']['appCode'];
+                        $arrGenderAS[$strAppCode]['M']+=$intTotalMale;
+                        $arrGenderAS[$strAppCode]['F']+=$intTotalFemale;
+                        $str .= $i>0?',':'';
+                        $str .= $arrGenderAS[$strAppCode]['M'];
+                        $i++;
+                    endforeach; 
+                    $str .= ']';
+                    echo $str;
+                ?>
+                // data: [0,0,0,0] 
+                //data: [110, 105, 85, 93, 80]
+            }, {
+                type: 'column',
+                name: 'Female',
+                /*data: [<? for($i=0;$i<count($categories);$i++){
+                        echo ($femaledata[$i]=="")?0:$femaledata[$i];
+                        $totalfemale+=$femaledata[$i];
+                        if($i<count($categories)-1) echo ",";
+                    }?>]
+                    */
+                <?php 
+                    $str = 'data: [';
+                    $i=0;
+                    foreach($arrAS as $row):
+                        $strAppCode = $arrGenderChart[$row]['F']['appCode'];
+
+                        $arrGenderAS[$strAppCode]['F']+=$intTotalFemale;
+                        $str .= $i>0?',':'';
+                        $str .= $arrGenderAS[$strAppCode]['F'];
+                        $i++;
+                    endforeach; 
+                    $str .= ']';
+                    echo $str;
+                ?>
+                // data: [0,0,0,0]     
+                //data: [145, 122, 90, 106, 98]
+            }, //{
+                //type: 'spline',
+                //name: 'Average',
+                /*
+                data: [<? for($i=0;$i<count($categories);$i++){
+                        echo number_format((($maledata[$i]+$femaledata[$i])/2),2);
+                        if($i<count($categories)-1) echo ",";
+                    }?>]
+                */
+                       
+                //data: [127.5, 113.5, 87.5, 99.5, 89]
+            //}, 
+        {
+                type: 'pie',
+                name: 'Total',
+                data: [{
+                    name: 'Male',
+                    y: <?=$arrGender['intTotalMale']?>,
+                    color: Highcharts.getOptions().colors[0]  // Male color
+                }, {
+                    name: 'Female',
+                    y: <?=$arrGender['intTotalFemale']?>,
+                    color: Highcharts.getOptions().colors[1] // Female color
+                }],
+                center: [50, 30],
+                size: 75,
+                showInLegend: false,
+                dataLabels: {
+                    enabled: false
+                }
+            }]
+        });
+  });//end document ready
+</script>
 <script src="<?=base_url('assets/plugins/counterup/jquery.waypoints.min.js')?>" type="text/javascript"></script>
 <script src="<?=base_url('assets/plugins/counterup/jquery.counterup.min.js')?>" type="text/javascript"></script>
--- application/modules/pds/controllers/Pds.php
+++ application/modules/pds/controllers/Pds.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Pds extends MY_Controller 
 {
 	var $arrData;
+
 	function __construct() 
 	{
         parent::__construct();
@@ -12,46 +13,8 @@ class Pds extends MY_Controller
 
 	public function index()
 	{
-		$arrData['arrEmployees'] = $this->Hr_model->getData();
-		//plantilla chart
-		$arrPlantillaChart = $this->chart_model->plantilla_positions();
-		$intFilled=0;$intVacant=0;
-		foreach($arrPlantillaChart->result_array() as $row):
-			if($row['empNumber']!='')
-				$intFilled+=1;
-			else
-				$intVacant+=1;
-		endforeach;
-		$arrData['intFilled']=$intFilled;
-		$arrData['intVacant']=$intVacant;
-		//gender chart
-		$arrAS = $this->Hr_model->appointment_status();
-		$arrASFull = $this->Hr_model->appointment_status(TRUE);
-		//print_r($arrASFull);
-		foreach($arrASFull as $row):
-			//echo $row."<br>";
-			$arrGenderChart[$row]['M'] = $this->chart_model->gender_appointment($row,'M');
-			$arrGenderChart[$row]['M']['appCode'] = appstatus_code($row);
-			$arrGenderChart[$row]['F'] = $this->chart_model->gender_appointment($row,'F');
-			$arrGenderChart[$row]['F']['appCode'] = appstatus_code($row);
-			//echo "<br>";
-			//$arrGenderChart[$row] = $this->chart_model->gender_appointment($row);
-		endforeach;
-		//total male
-		$i=0;$arrGender=array('intTotalMale'=>0,'intTotalFemale'=>0);
-        foreach($arrASFull as $row):
-        	//echo $row.'=>M=>'.$arrGenderChart[$row]['M'][0]['total']."<br>";
-        	//echo $row.'=>F=>'.$arrGenderChart[$row]['F'][0]['total'];
-        	//echo '<br><br>';
-            $arrGender['intTotalMale'] += $arrGenderChart[$row]['M'][0]['total'];
-            $arrGender['intTotalFemale'] += $arrGenderChart[$row]['F'][0]['total'];
-        endforeach;
-
-		$arrData['arrAS'] = $arrAS;
-		$arrData['arrASFull'] = $arrASFull;
-		$arrData['arrGender'] = $arrGender;
-		$arrData['arrGenderChart'] = $arrGenderChart;
-		$this->template->load('template/template_view','pds/default_view',$arrData);
+		$this->arrData['arrEmployees'] = $this->Hr_model->getData();
+		$this->template->load('template/template_view','pds/default_view',$this->arrData);
 	}
 
     public function edit_personal()
