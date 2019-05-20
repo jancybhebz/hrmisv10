be54a55530400da39a439dc3fb6d312d6bfdd2b6
Mai <mychll31@gmail.com>
2019-05-20 23:10:37 +0800

    update view pds pdf

--- application/modules/pds/views/201/view_employee.php
+++ application/modules/pds/views/201/view_employee.php
@@ -86,7 +86,8 @@ $arrData = $arrData[0];?>
                                                         </tbody>
                                                     </table>
                                                     <div>
-                                                        <a href="<?=base_url('employee/reports/generate/?rpt=reportPDSupdate')?>"/><button type="button" value="reportPDSupdate" class="btn blue">Print Personal Data Sheet</button></a>
+                                                        <button type="button" value="reportPDSupdate" class="btn blue" data-toggle="modal"
+                                                            href="#print-preview-pds-modal">Print Personal Data Sheet</button>
                                                     </div>
                                                 </div>
                                             </div>
@@ -278,6 +279,33 @@ $arrData = $arrData[0];?>
         </div>
     </div>
 </div>
+
+<!-- begin print-preview modal -->
+<div id="print-preview-pds-modal" class="modal fade" aria-hidden="true">
+    <div class="modal-dialog" style="width: 75%;">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                <h4 class="modal-title bold">Personal Data Sheet</h4>
+            </div>
+            <div class="modal-body">
+                <div class="row form-body">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <embed src="<?=base_url('employee/reports/generate/?rpt=reportPDSupdate')?>" frameborder="0" width="100%" height="500px">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <a href="<?=base_url('employee/reports/generate/?rpt=reportPDSupdate')?>"
+                    class="btn blue btn-sm" target="_blank"> <i class="glyphicon glyphicon-resize-full"> </i> Open New Tab</a>
+                <button type="button" class="btn dark btn-sm" data-dismiss="modal"> <i class="icon-ban"> </i> Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end print-preview modal -->
 
 <?php load_plugin('js',array('select'));?>
 <script>
