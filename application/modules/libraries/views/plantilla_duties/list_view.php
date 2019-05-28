b99e5e8177c01a137f4ff4ef866440b362709f04
Rose Anne <rosegrefaldeo01@gmail.com>
2019-05-28 15:50:17 +0700

    itemNumber under duties

--- application/modules/libraries/views/plantilla_duties/list_view.php
+++ application/modules/libraries/views/plantilla_duties/list_view.php
@@ -36,6 +36,7 @@ Copyright Notice:   Copyright(C)2018 by the DOST Central Office - Information Te
                     <thead>
                         <tr>
                             <th> No. </th>
+                            <th> Item Number </th>
                             <th> Duties </th>
                     </thead>
                     <tbody>
@@ -44,6 +45,7 @@ Copyright Notice:   Copyright(C)2018 by the DOST Central Office - Information Te
                     foreach($arrPDuties as $duty):?>
                         <tr class="odd gradeX">
                             <td> <?=$i?> </td>
+                            <td> <?=$duty['itemNumber']?> </td>
                             <td> <?=$duty['itemDuties']?> </td>
                         </tr>
                     <?php 
