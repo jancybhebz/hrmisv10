8a53043a2acfb76a736220b77245b2c4d8375a85
Mai <mychll31@gmail.com>
2019-06-02 22:41:04 +0800

    process history and published / unpublished function

--- application/modules/finance/views/payroll/process_history.php
+++ application/modules/finance/views/payroll/process_history.php
@@ -47,6 +47,7 @@
                                 <div class="form-group" style="display: inline-flex;">
                                     <label style="padding: 6px;">Month</label>
                                     <select class="bs-select form-control" name="month">
+                                        <option value="all">All</option>
                                         <?php foreach (range(1, 12) as $m): ?>
                                             <option value="<?=sprintf('%02d', $m)?>"
                                                 <?php 
@@ -85,6 +86,7 @@
                             <thead>
                                 <tr>
                                     <th width="50px"> No </th>
+                                    <th> Process </th>
                                     <th> Employee </th>
                                     <th> Month </th>
                                     <th> Year </th>
@@ -95,23 +97,31 @@
                                 </tr>
                             </thead>
                             <tbody>
+                                <?php $no=1;foreach($arrprocess as $process): ?>
                                 <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td>
+                                    <td align="center"><?=$no++?></td>
+                                    <td align="center"><?=$process['processCode']?></td>
+                                    <td align="center"><?=$process['appointmentDesc']?></td>
+                                    <td align="center"><?=date('F', mktime(0, 0, 0, $process['processMonth'], 10))?></td>
+                                    <td align="center"><?=$process['processYear']?></td>
+                                    <td align="center"><?=$process['processDate']?></td>
+                                    <td><?=$process['details']?></td>
+                                    <td><?=employee_name($process['empNumber'])?></td>
+                                    <td style="width: 250px; white-space: nowrap;">
                                         <a href="javascript:;" class="btn btn-sm blue" id="btnreprocess">
                                             <i class="fa fa-refresh"></i> Reprocessed</a>
                                         <a href="javascript:;" class="btn btn-sm grey-cascade" id="btnreprocess">
                                             <i class="fa fa-file-o"></i> Reports</a>
-                                        <a href="javascript:;" class="btn btn-sm green" id="btnreprocess">
-                                            <i class="fa fa-check"></i> Publish</a>
+                                        <?php if($process['publish'] == 1): ?>
+                                            <a href="javascript:;" class="btn btn-sm green-meadow" id="btnunpublish" data-procid="<?=$process['processID']?>">
+                                                <i class="fa fa-check"></i> Unpublish</a>
+                                        <?php else: ?>
+                                            <a href="javascript:;" class="btn btn-sm green" id="btnpublish" data-procid="<?=$process['processID']?>">
+                                                <i class="fa fa-check"></i> Publish</a>
+                                        <?php endif; ?>
                                     </td>
                                 </tr>
+                                <?php endforeach; ?>
                             </tbody>
                         </table>
                     </div>
@@ -120,6 +130,8 @@
         </div>
     </div>
 </div>
+
+<?php include 'modals/process_history_modal.php'; ?>
 
 <?=load_plugin('js', array('select','datatables'))?>
 
@@ -131,5 +143,22 @@
                 $('#tblprocess-history').css('visibility', 'visible');
                 $('#div-body').css('visibility', 'visible');
             }} );
+
+        $('#tblprocess-history').on('click', 'a#btnunpublish', function() {
+            $('.modal-title').html('<b>Unpublish Process</b>');
+            $('#txtprocess_id').val($(this).data('procid'));
+            $('#txtpulish_val').val(0);
+            $('#spanpublish').html('unpublish');
+            $('#publish_process').modal('show');
+        });
+
+        $('#tblprocess-history').on('click', 'a#btnpublish', function() {
+            $('.modal-title').html('<b>Publish Process</b>');
+            $('#txtprocess_id').val($(this).data('procid'));
+            $('#txtpulish_val').val(1);
+            $('#spanpublish').html('pulish');
+            $('#publish_process').modal('show');
+        });
+
     });
 </script>
