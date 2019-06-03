a4ac7461a59269a12a42f48f25905c9a0f99d34f
MYCHLL <mychll31@gmail.com>
2019-06-03 16:50:26 +0800

    add request (template) for officer task

--- application/modules/officer/views/modals/_request_details.php
+++ application/modules/officer/views/modals/_request_details.php
@@ -1,4 +1,4 @@
-<div id="modal-viewRequest" class="modal fade" aria-hidden="true">
+<div id="request_leave" class="modal fade" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -36,13 +36,6 @@
                                 <span class="help-block"></span>
                             </div>
                         </div>
-                        <!-- <div class="form-group">
-                            <label class="col-md-3 control-label">Specific Leave</label>
-                            <div class="col-md-8">
-                                <input type="text" class="form-control" id="txtreq_spe_leave" name="txtreq_spe_leave" disabled>
-                                <span class="help-block"></span>
-                            </div>
-                        </div> -->
                         <div class="form-group">
                             <label class="col-md-3 control-label">Date From</label>
                             <div class="col-md-8">
@@ -64,14 +57,14 @@
                                 <span class="help-block"></span>
                             </div>
                         </div>
-                        <div class="form-group">
+                        <div class="form-group txtreq_patient">
                             <label class="col-md-3 control-label">Out/In Patient</label>
                             <div class="col-md-8">
                                 <input type="text" class="form-control" id="txtreq_patient" name="txtreq_patient">
                                 <span class="help-block"></span>
                             </div>
                         </div>
-                        <div class="form-group">
+                        <div class="form-group txtreq_location">
                             <label class="col-md-3 control-label">Location</label>
                             <div class="col-md-8">
                                 <input type="text" class="form-control" id="txtreq_location" name="txtreq_location">
@@ -103,6 +96,209 @@
                 </div>
                 <div class="modal-footer">
                     <button type="submit" id="btncertify" class="btn btn-sm green"><i class="icon-check"> </i> Certify</button>
+                    <button type="button" class="btn btn-sm btn-primary" data-dismiss="modal"><i class="icon-ban"> </i> Cancel</button>
+                </div>
+            <?=form_close()?>
+        </div>
+    </div>
+</div>
+
+<div id="request_ob" class="modal fade" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                <h4 class="modal-title"></h4>
+            </div>
+            <?=form_open('employee/requests/cancel_request', array('class' => 'form-horizontal'))?>
+                <div class="modal-body">
+                    <div class="form-body">
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Request ID</label>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control" id="txtob_id" name="txtob_id" disabled>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Employee Number</label>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control" id="txtob_empno" name="txtob_empno" disabled>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Employee Name</label>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control" id="txtob_empname" name="txtob_empname" disabled>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Official Business</label>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control" id="txtob_type" name="txtob_type" disabled>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Place</label>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control" id="txtob_place" name="txtob_place" disabled>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Purpose </label>
+                            <div class="col-md-8">
+                                <textarea class="form-control" id="txtob_purpose" name="txtob_purpose" disabled></textarea>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label" style="line-height: 5px;">With Meal</label>
+                            <div class="col-md-8">
+                                <label><input type="checkbox" id="txtob_wmeal" name="txtob_wmeal" disabled></label>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Date From</label>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control" id="txtob_dfrom" name="txtob_dfrom" disabled>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Date To</label>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control" id="txtob_dto" name="txtob_dto" disabled>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Time in</label>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control" id="txtob_time_in" name="txtob_time_in" disabled>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Time out</label>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control" id="txtob_time_out" name="txtob_time_out" disabled>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Status of Request</label>
+                            <div class="col-md-8">
+                                <select class="bs-select form-control" name="selob_stat" id="selob_stat"></select>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Remarks</label>
+                            <div class="col-md-8">
+                                <textarea class="form-control" id="txtob_remarks" name="txtob_remarks"></textarea>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" id="btnobcertify" class="btn btn-sm green"><i class="icon-check"> </i> Certify</button>
+                    <button type="button" class="btn btn-sm btn-primary" data-dismiss="modal"><i class="icon-ban"> </i> Cancel</button>
+                </div>
+            <?=form_close()?>
+        </div>
+    </div>
+</div>
+
+<div id="request_to" class="modal fade" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                <h4 class="modal-title"></h4>
+            </div>
+            <?=form_open('employee/requests/cancel_request', array('class' => 'form-horizontal'))?>
+                <div class="modal-body">
+                    <div class="form-body">
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Request ID</label>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control" id="txtto_id" name="txtto_id" disabled>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Employee Number</label>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control" id="txtto_empno" name="txtto_empno" disabled>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Employee Name</label>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control" id="txtto_empname" name="txtto_empname" disabled>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Destination </label>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control" id="txtto_desti" name="txtto_desti" disabled>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Date From</label>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control" id="txtto_dfrom" name="txtto_dfrom" disabled>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Date To</label>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control" id="txtto_dto" name="txtto_dto" disabled>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Purpose </label>
+                            <div class="col-md-8">
+                                <textarea class="form-control" id="txtto_purpose" name="txtto_purpose" disabled></textarea>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label" style="line-height: 5px;">With Meal</label>
+                            <div class="col-md-8">
+                                <label><input type="checkbox" id="txtto_wmeal" name="txtto_wmeal" disabled></label>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Status of Request</label>
+                            <div class="col-md-8">
+                                <select class="bs-select form-control" name="selto_stat" id="selto_stat"></select>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Remarks</label>
+                            <div class="col-md-8">
+                                <textarea class="form-control" id="txtto_remarks" name="txtto_remarks"></textarea>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" id="btntocertify" class="btn btn-sm green"><i class="icon-check"> </i> Certify</button>
                     <button type="button" class="btn btn-sm btn-primary" data-dismiss="modal"><i class="icon-ban"> </i> Cancel</button>
                 </div>
             <?=form_close()?>
