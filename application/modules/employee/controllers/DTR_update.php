43867278b0ef14bda3609c60f6d744a409d2cb57
Rose Anne <rosegrefaldeo01@gmail.com>
2019-06-10 10:11:35 +0700

    file name changes

--- application/modules/employee/controllers/DTR_update.php
+++ application/modules/employee/controllers/DTR_update.php
@@ -9,7 +9,7 @@ Copyright Notice:   Copyright(C)2018 by the DOST Central Office - Information Te
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class DTR_update extends MY_Controller {
+class Dtr_update extends MY_Controller {
 
 	var $arrData;
 
