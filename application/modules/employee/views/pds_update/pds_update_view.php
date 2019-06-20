ec40b27e1733a5ad3f09aef65e84f542d2c4e0d5
Maychell Alcorin <mychll31@gmail.com>
2019-06-20 03:40:12 +0000

    pds_update_view.php edited online with Bitbucket

--- application/modules/employee/views/pds_update/pds_update_view.php
+++ application/modules/employee/views/pds_update/pds_update_view.php
@@ -586,7 +586,7 @@ Copyright Notice:   Copyright(C)2018 by the DOST Central Office - Information Te
             <div class="col-sm-8">
                 <div class="form-group">
                     <label class="control-label">Basic Education/Degree/Course :  </label>
-                    <select type="text" class="form-control" name="strDegree" value="<?=!empty($this->session->userdata('strDegree'))?$this->session->userdata('strDegree'):''?>" required>
+                    <select type="text" class="form-control" name="strDegree" value="<?=!empty($this->session->userdata('strDegree'))?$this->session->userdata('strDegree'):''?>">
                              <option value="">Select</option>
                             <?php foreach($arrCourse as $course)
                             {
