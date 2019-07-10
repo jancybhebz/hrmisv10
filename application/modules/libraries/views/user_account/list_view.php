c39059ef2d7d10c595d7a15a54e1f8ceee7fb014
Rose Anne <rosegrefaldeo01@gmail.com>
2019-07-10 08:50:29 +0700

    useraccount breadcrumbs and loading

--- application/modules/libraries/views/user_account/list_view.php
+++ application/modules/libraries/views/user_account/list_view.php
@@ -8,8 +8,30 @@ Copyright Notice:   Copyright(C)2018 by the DOST Central Office - Information Te
 ?>
 <?php load_plugin('css',array('datepicker','datatables'));?>
 
+<!-- BEGIN PAGE BAR -->
+<div class="page-bar">
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="<?=base_url('home')?>">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <span>Libraries</span>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <span>User Account</span>
+        </li>
+    </ul>
+</div>
+<!-- END PAGE BAR -->
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-lg-12 col-md-12 col-sm-12">
+       &nbsp;
+    </div>
+</div>
+<div class="row">
+  <div class="col-md-12">
         <!-- BEGIN EXAMPLE TABLE PORTLET-->
         <div class="portlet light bordered">
             <div class="portlet-title">
@@ -19,7 +41,8 @@ Copyright Notice:   Copyright(C)2018 by the DOST Central Office - Information Te
                 </div>
                 
             </div>
-            <div class="portlet-body">
+            <div class="loading-image"><center><img src="<?=base_url('assets/images/spinner-blue.gif')?>"></center></div>
+            <div class="portlet-body" id="div-useraccount" style="display: none">
                 <div class="table-toolbar">
                     <div class="row">
                         <div class="col-md-6">
@@ -33,13 +56,13 @@ Copyright Notice:   Copyright(C)2018 by the DOST Central Office - Information Te
                 <table class="table table-striped table-bordered table-hover table-checkable order-column" id="libraries_user_account">
                     <thead>
                         <tr>
-                            <th> No. </th>
+                            <th style="width: 100px;text-align:center;"> No. </th>
                             <th> Employee Number </th>
                             <th> Employee Name </th>
                             <th> Username </th>
                             <th> Access Level </th>
                             <th> User Permission </th>
-                            <th> Action </th>
+                            <th class="no-sort" style="text-align: center;"> Actions </th>
                         </tr>
                     </thead>
                     <tbody>
@@ -78,7 +101,7 @@ Copyright Notice:   Copyright(C)2018 by the DOST Central Office - Information Te
                                 }
                             ?>
                             </td>
-                            <td>
+                            <td width="150px" style="white-space: nowrap;">
                                 <a href="<?=base_url('libraries/user_account/edit/'.$row['empNumber'])?>"><button class="btn btn-sm btn-success"><span class="fa fa-edit" title="Edit"></span> Edit</button></a>
                                 <a href="<?=base_url('libraries/user_account/delete/'.$row['empNumber'])?>"><button class="btn btn-sm btn-danger"><span class="fa fa-trash" title="Delete"></span> Delete</button></a>
                                 <a href="<?=base_url('libraries/user_account/reset/'.$row['empNumber'])?>"><button class="btn btn-sm btn-blue"><span class="fa fa-refresh" title="Reset"></span> Reset Password</button></a>
@@ -100,6 +123,10 @@ Copyright Notice:   Copyright(C)2018 by the DOST Central Office - Information Te
 
 <script>
     $(document).ready(function() {
-        Datatables.init('libraries_user_account');
-  });
+        $('#libraries_user_account').dataTable( {
+            "initComplete": function(settings, json) {
+                $('.loading-image').hide();
+                $('#div-useraccount').show();
+            }} );
+    });
 </script>
