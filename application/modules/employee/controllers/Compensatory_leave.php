74c170527b8260f905c062eb2cefdfeacde0ebe4
Rose Anne <rosegrefaldeo01@gmail.com>
2019-08-05 08:44:45 +0700

    cto,pds and reports

--- application/modules/employee/controllers/Compensatory_leave.php
+++ application/modules/employee/controllers/Compensatory_leave.php
@@ -60,7 +60,7 @@ class Compensatory_leave extends MY_Controller {
 					if(count($blnReturn)>0)
 					{	
 						log_action($this->session->userdata('sessEmpNo'),'HR Module','tblEmpRequest','Added '.$dtmComLeave.' Official Business',implode(';',$arrData),'');
-						$this->session->set_flashdata('strMsg','Request has been submitted.');
+						$this->session->set_flashdata('strSuccessMsg','Request has been submitted.');
 					}
 					redirect('employee/compensatory_leave');
 				}
