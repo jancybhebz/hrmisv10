52871a77a0382c4361c090ea319ba1d63e8d9643
MYCHLL <mychll31@gmail.com>
2019-09-10 08:59:45 +0800

    update leave balance set

--- application/modules/hr/views/attendance/attendance_summary/_leave_balance_set.php
+++ application/modules/hr/views/attendance/attendance_summary/_leave_balance_set.php
@@ -21,21 +21,21 @@
                                 <label class="control-label">Starting Balance <span class="required"> * </span></label>
                                 <div class="input-icon right">
                                     <i class="fa fa-warning tooltips i-required"></i>
-                                    <input type="number" class="form-control" name="vl_start" value="0.000">
+                                    <input type="number" class="form-control" name="vl_start" value="0.000" step="any">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class="control-label">Absent Undertime With Pay <span class="required"> * </span></label>
                                 <div class="input-icon right">
                                     <i class="fa fa-warning tooltips i-required"></i>
-                                    <input type="number" class="form-control" name="vl_ut_wpay" value="0.000">
+                                    <input type="number" class="form-control" name="vl_ut_wpay" value="0.000" step="any">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class="control-label">Absent Undertime Without Pa <span class="required"> * </span></label>
                                 <div class="input-icon right">
                                     <i class="fa fa-warning tooltips i-required"></i>
-                                    <input type="number" class="form-control" name="vl_ut_wopay" value="0.000">
+                                    <input type="number" class="form-control" name="vl_ut_wopay" value="0.000" step="any">
                                 </div>
                             </div>
                             <!-- end vacation leave -->
@@ -47,21 +47,21 @@
                                 <label class="control-label">Starting Balance <span class="required"> * </span></label>
                                 <div class="input-icon right">
                                     <i class="fa fa-warning tooltips i-required"></i>
-                                    <input type="number" class="form-control" name="sl_start" value="0.000">
+                                    <input type="number" class="form-control" name="sl_start" value="0.000" step="any">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class="control-label">Absent Undertime With Pay <span class="required"> * </span></label>
                                 <div class="input-icon right">
                                     <i class="fa fa-warning tooltips i-required"></i>
-                                    <input type="number" class="form-control" name="sl_ut_wpay" value="0.000">
+                                    <input type="number" class="form-control" name="sl_ut_wpay" value="0.000" step="any">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class="control-label">Absent Undertime Without Pa <span class="required"> * </span></label>
                                 <div class="input-icon right">
                                     <i class="fa fa-warning tooltips i-required"></i>
-                                    <input type="number" class="form-control" name="sl_ut_wopay" value="0.000">
+                                    <input type="number" class="form-control" name="sl_ut_wopay" value="0.000" step="any">
                                 </div>
                             </div>
                             <!-- end sick leave -->
@@ -73,7 +73,7 @@
                                 <label class="control-label">Offset Balance <span class="required"> * </span></label>
                                 <div class="input-icon right">
                                     <i class="fa fa-warning tooltips i-required"></i>
-                                    <input type="number" class="form-control" name="off_bal" value="0">
+                                    <input type="number" class="form-control" name="off_bal" value="0" step="any">
                                     <span class="help-block small">in Minutes</span>
                                 </div>
                             </div>
@@ -81,14 +81,14 @@
                                 <label class="control-label">Forced Leave Balance <span class="required"> * </span></label>
                                 <div class="input-icon right">
                                     <i class="fa fa-warning tooltips i-required"></i>
-                                    <input type="number" class="form-control" name="fl_bal" value="0">
+                                    <input type="number" class="form-control" name="fl_bal" value="0" step="any">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class="control-label">Privilege Leave Balance <span class="required"> * </span></label>
                                 <div class="input-icon right">
                                     <i class="fa fa-warning tooltips i-required"></i>
-                                    <input type="number" class="form-control" name="pl_bal" value="0">
+                                    <input type="number" class="form-control" name="pl_bal" value="0" step="any">
                                 </div>
                             </div>
                             <!-- end others leave -->
