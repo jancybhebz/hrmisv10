4ae8d069b43a2f62360cc1b2f2992b68803c0ced
MYCHLL <mychll31@gmail.com>
2019-09-10 09:02:41 +0800

    update leave balance set

--- application/modules/hr/views/attendance/attendance_summary/_leave_balance_update.php
+++ application/modules/hr/views/attendance/attendance_summary/_leave_balance_update.php
@@ -94,8 +94,6 @@
                                 <i class="fa fa-pencil"></i> &nbsp;Update Leave Balance</button>
                             <button class="btn blue" data-toggle="modal" data-backdrop="static" data-keyboard="false" href="#modal-rollback" id="btn-rollback">
                                 <i class="fa fa-refresh"></i> &nbsp;Rollback</button>
-                            <a class="btn grey-mint" href="<?=base_url('hr/attendance_summary/leave_balance/').$arrData['empNumber'].'?month='.$month.'&yr='.$yr?>">
-                                <i class="icon-arrow-left"></i> &nbsp;Back</a>
                         </p>
                     </div>
                 </div>
