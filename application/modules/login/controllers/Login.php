ae73b0d42c64087bfec8abcb9ea97d67712454a3
MYCHLL <mychll31@gmail.com>
2019-10-23 14:05:39 +0800

    fix empty login - username and password

--- application/modules/login/controllers/Login.php
+++ application/modules/login/controllers/Login.php
@@ -14,15 +14,20 @@ class Login extends MY_Controller {
 
 		$arrPost = $this->input->post();
 		if(!empty($arrPost)):
-			$this->load->model('login_model');
-			$arrUser = $this->login_model->authenticate($arrPost['strUsername'],$arrPost['strPassword']);
-			if(!empty($arrUser)):
-				
-				$this->set_session_login_data($arrUser[0]['empNumber'],$arrUser[0]['userLevel'],$arrUser[0]['userPermission'],$arrUser[0]['accessPermission'],$arrUser[0]['userName'],$arrUser[0]['userPassword'],$arrUser[0]['firstname'].' '.$arrUser[0]['surname'],$arrUser[0]['assignedGroup'],$arrUser[0]['is_assistant']);
-				redirect('home/index');
-			else:
-				$this->session->set_flashdata('strErrorMsg','Invalid username/password.');
+			if($arrPost['strUsername']=='' || $arrPost['strPassword'] ==''):
+				$this->session->set_flashdata('strErrorMsg','Please enter Username/Password.');
 				$this->load->view('login_view');
+			else:
+				$this->load->model('login_model');
+				$arrUser = $this->login_model->authenticate($arrPost['strUsername'],$arrPost['strPassword']);
+				if(!empty($arrUser)):
+					
+					$this->set_session_login_data($arrUser[0]['empNumber'],$arrUser[0]['userLevel'],$arrUser[0]['userPermission'],$arrUser[0]['accessPermission'],$arrUser[0]['userName'],$arrUser[0]['userPassword'],$arrUser[0]['firstname'].' '.$arrUser[0]['surname'],$arrUser[0]['assignedGroup'],$arrUser[0]['is_assistant']);
+					redirect('home/index');
+				else:
+					$this->session->set_flashdata('strErrorMsg','Invalid username/password.');
+					$this->load->view('login_view');
+				endif;
 			endif;
 		else:
 			if(!$session):
