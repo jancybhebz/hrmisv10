f116a12550d8b5afbb6e71bba24fc566bfb37635
fnvperez <perez.francisnikko@gmail.com>
2020-05-21 12:12:12 +0800

    resized modal

--- application/modules/dtrkiosk/views/_dtr_modal.php
+++ application/modules/dtrkiosk/views/_dtr_modal.php
@@ -5,7 +5,7 @@
 </style>
 
 <div id="dtr-modal" class="modal fade" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered"  style="width: 70%;">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
