c4b05d96f9b0f841beb352df996d5ee8afc56066
fnvperez <perez.francisnikko@gmail.com>
2021-02-05 08:43:57 +0800

    fixed missing end div in other info

--- application/modules/pds/views/201/view_employee.php
+++ application/modules/pds/views/201/view_employee.php
@@ -262,6 +262,7 @@
                                                     <div class="scroller" style="height:350px;" data-always-visible="1" data-rail-visible="1" data-rail-color="red" data-handle-color="green">
                                                         <?php $this->load->view('_other_info_view.php'); ?>
                                                     </div>
+                                                    </div>
                                                 </div>
                                                 <!-- end other info -->
 
